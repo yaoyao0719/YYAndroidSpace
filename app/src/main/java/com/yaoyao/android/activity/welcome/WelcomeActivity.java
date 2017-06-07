d81503693173acc1535515c8c56f5208ca1c4e59
姚瑶 <229847401@qq.com>
2017-06-07 15:32:21 +0800

    整理版本号

---
+++ app/src/main/java/com/yaoyao/android/activity/welcome/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.yaoyao.android.activity.welcome;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.yaoyao.android.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
